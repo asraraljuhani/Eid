1373c20212d6f86dcaa072249f749717ad6df0a1
AsrarAljuhani <asrarbaljuhani@gmail.com>
2020-05-23 01:24:32 +0300

    change style

--- eid2020.js
+++ eid2020.js
@@ -1,12 +1,14 @@
 
-x=270;
-y=450;
-name="أسرار";
+x=150;
+y=385;
+name="";
 const drawImage = (url,canvasNumber,name,x,y) => {
     let canvas = document.getElementById("canvas"+canvasNumber);
 let ctx = canvas.getContext("2d"); 
     let image = new Image();
     image.src = url;
+    // canvas.width=image.width;
+    // canvas.height=image.height;
     var hRatio = canvas.width  / image.width    ;
     var vRatio =  canvas.height / image.height  ;
     var ratio  = Math.max ( hRatio, vRatio );
@@ -16,7 +18,7 @@ let ctx = canvas.getContext("2d");
     image.onload = () => {
        ctx.drawImage(image, 0, 0, image.width,    image.height,     // source rectangle
         0, 0, canvas.width, canvas.height);
-        ctx.font = "30px Comic Sans MS";
+        ctx.font = "15px Tajawal";
         ctx.fillStyle = "#fff";
         ctx.direction = "rtl";
         ctx.textAlign = 'center';
@@ -24,14 +26,34 @@ let ctx = canvas.getContext("2d");
    
     }
 }
-drawImage("images/Eid-1.jpg","1","",x,y);
-drawImage("images/Eid-2.jpg","2","",x,y);
+// drawImage("images/img-f.jpeg","1","",x,y);
+// drawImage("images/img-m.jpeg","2","",x,y);
+drawImage("images/img-f.jpeg","1",name,x,y);
+drawImage("images/img-m.jpeg","2",name,x,y);
 function addName(){
     let name=document.getElementById("name").value;
-    drawImage("images/Eid-1.jpg","1",name,x,y);
-    drawImage("images/Eid-2.jpg","2",name,x,y);
+    drawImage("images/img-f.jpeg","1",name,x,y);
+    drawImage("images/img-m.jpeg","2",name,x,y);
 
 }
+
+changeImg = (value)=> {
+console.log(value);
+}
+
+document.getElementById("img-f").style.display = "none";
+
+document.getElementById("customRadioInline1").addEventListener("click", function(){
+    document.getElementById("img-f").style.display = "none";
+    document.getElementById("img-m").style.display = "block";
+    drawImage("images/img-m.jpeg","2",name,x,y);
+  });
+  document.getElementById("customRadioInline2").addEventListener("click", function(){
+    document.getElementById("img-m").style.display = "none";
+    document.getElementById("img-f").style.display = "block";
+    drawImage("images/img-f.jpeg","1",name,x,y);
+  });
+
 saveImg1=function (el){
     let canvas = document.getElementById("canvas1");
     let img=canvas.toDataURL("image/jpg");
@@ -44,5 +66,6 @@ saveImg2=function (el){
     console.log(img);
   el.href = img;
 }
+
 
 
